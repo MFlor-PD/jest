1aff0a5bf1a141aec5f8162686538ccbbfa76400
Flor <mariaflorenciaperezd@gmail.com>
2025-05-05 21:01:53 +0200

    errores

--- product.test.js
+++ product.test.js
@@ -1,10 +1,10 @@
-const { resetProducts,  addProduct, removeProduct, getProducts, getProduct, updateProduct} = require('./product');
-beforeEach(() => {
+//const { resetProducts,  addProduct, removeProduct, getProducts, getProduct, updateProduct} = require('./product');
+/*beforeEach(() => {
     resetProducts();
 });
 afterEach(() => {
     resetProducts();
-});
+});*/
 // El primer test que vamos a hacer es el de crear un producto. **addProduct**
 //El siguiente test que vamos a hacer es el de eliminar un producto. Para ello, vamos a usar la función **removeProduct**. 
 //Test obtener producto. getProduct
